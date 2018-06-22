6305516328e93149bc8dd96392363c4b33a01218
David <david@pinkiering.com>
2018-06-22 10:12:37 +0200

    Enable node and ecma 2017 in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   env: {
     browser: true,
+    node: true,
     es6: true
   },
   extends: 'eslint:recommended',
   parserOptions: {
+    ecmaVersion: 2017,
     ecmaFeatures: {
       jsx: true
     },
