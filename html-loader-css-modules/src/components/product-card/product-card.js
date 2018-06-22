d1d285c560284ac99326f8a7074336b96e2e98ba
David <david@pinkiering.com>
2018-06-22 10:11:47 +0200

    Modify App
    - Clean up CSS
    - Adjust JS

--- html-loader-css-modules/src/components/product-card/product-card.js
+++ html-loader-css-modules/src/components/product-card/product-card.js
@@ -1,39 +1,64 @@
 import css from './product-card.css';
-import { r } from '../../../lib/renderer';
+import theme from '../../theme.css';
+import { r } from '../../../lib/renderer'; // jsx handler
 
 export class ProductCard extends HTMLElement {
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: 'open' });
+    css.insertInto(this.shadow);
+    theme.insertInto(this.shadow);
     this.shadow.appendChild(this.render());
+    this.button = this.shadow.querySelector('button');
+    this.productName = this.shadow.querySelector('h1');
+    this.img = this.shadow.querySelector('img');
+    this.onClick = this.onClick.bind(this);
   }
 
   static get observedAttributes() {
-    return ['label', 'name'];
+    return ['label', 'name', 'source'];
   }
 
   connectedCallback() {
-    css.insertInto(this.shadow);
-    this.button = this.shadow.querySelector('button');
-    this.handleClick && this.button.addEventListener('click', this.handleClick);
-    this.productName = this.shadow.querySelector('h1');
+    this.handleClick &&
+      this.button.addEventListener('click', this.onClick);
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
-    this.h1.textContent = newValue;
+  disconnectedCallback() {
+    this.removeEventListener('click', this.onClick);
+  }
+
+  onClick() {
+    this.handleClick && this.handleClick(this.sku);
+  }
+
+  attributeChangedCallback(name, _, newValue) {
+    switch (name) {
+      case 'name':
+        this.h1.textContent = newValue;
+        break;
+      case 'label':
+        this.button.textContent = newValue;
+        break;
+      case 'source':
+        this.img.setAttribute('src', newValue);
+        break;
+    }
   }
 
   render() {
+    const { h2, font, button } = theme;
+    const { img } = css;
     return (
       <fragment>
-        <h1 ref={el => (this.h1 = el)} class={css.h1}>
-          {this.name || 'Product Name'}
-        </h1>
-        {this.source && <img src={this.source} />}
-        <p>
+        <h2 ref={el => (this.h1 = el)} class={h2}>
+          {this.name}
+        </h2>
+        <img class={img} src={this.source} />
+        <p class={font}>
           <slot />
         </p>
-        <button class={css.button}>Buy this product</button>
+        <button class={button}>Buy this product</button>
       </fragment>
     );
   }
